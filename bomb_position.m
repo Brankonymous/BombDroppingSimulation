c69a3d0ef39926b222245ef2a35b896a2c9ba4dd
Brankonymous <brankogrbic01@gmail.com>
2024-05-24 11:45:34 +0200

    Calculate bomb position and time it takes for reaching the drop point

---
+++ bomb_position.m
@@ -0,0 +1,12 @@
+function [dropTime, bombPosX] = bomb_position()
+    %% Calculate
+    [JET_H, JET_VEL, G, ~, ~, ~] = constants();
+
+    bombVel = JET_VEL;
+    bombPosYStart = JET_H;
+    bombPosYEnd = 0;
+
+    % calculate position and time of bomb drop
+    dropTime = sqrt(2 * bombPosYStart / G);
+    bombPosX = bombVel * dropTime;
+end
