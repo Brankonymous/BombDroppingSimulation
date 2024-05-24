65c23371c81917e8698116e3fd4ecd79ed0d62b3
Brankonymous <brankogrbic01@gmail.com>
2024-05-24 14:03:47 +0200

    Add tangent calculation

--- jet_position.m
+++ jet_position.m
@@ -6,35 +6,42 @@ jetX = 0;
 jetY = 0; 
 bombX = 0;
 bombY = bombPosY;
-v = BOMB_VEL; 
+v = JET_VEL; 
 t = dropTime;  
-
 r = MIN_RADIUS;
-plotT = linspace(0, dropTime, 1000);
-x = jetX + r - r * cos(v * plotT / r);
-y = jetY + r * sin(v * plotT / r);
+
 plot(0, 0, 'o', 'Color', 'green');
 hold on;
 plot(bombX, bombY, 'o', 'Color', 'red');
 hold on;
+
+% FIND OPTIMAL R
+distance = @(r, t) sqrt((jetX + r - r * cos(v * t / r) - bombX)^2 + (jetY + r * sin(v * t / r) - bombY)^2);
+optimal_r = fminbnd(@(r) -distance(r, t), MIN_RADIUS, 10000);
+fprintf('The optimal r is: %.4f\n', optimal_r);
+jetX = jetX + optimal_r - optimal_r * cos(v * t / optimal_r);
+jetY = jetY + optimal_r * sin(v * t / optimal_r);
+plot(jetX, jetY, 'o', 'Color', 'green');
+hold on;
+% FIND OPTIMAL R
+
+[tanX, tanY, phi] = findTangents(0, optimal_r, bombY);
+plot([tanX, bombX], [tanY, bombY], 'Color', 'black');
+hold on;
+timeToTangent = optimal_r / (pi-2*phi) / v;
+
+plotT = linspace(0, dropTime, 1000);
+x = optimal_r - optimal_r * cos(v * plotT / optimal_r);
+y = optimal_r * sin(v * plotT / optimal_r);
 plot(x, y, 'Color', 'blue');
 hold on;
 
-[tanX, tanY, phi] = findTangents(jetY, MIN_RADIUS, bombY);
-plot([tanX, bombX], [tanY, bombY], 'Color', 'black');
+fprintf('Drop time: %.2f seconds\n', t);
+fprintf('Time from O to T: %.2f seconds\n', timeToTangent);
+plotT = linspace(0, timeToTangent, 1000);
+x = optimal_r - optimal_r * cos(v * (t + plotT) / optimal_r);
+y = optimal_r * sin(v * (t + plotT) / optimal_r);
+plot(x, y, 'Color', 'red');
 hold on;
 
-timeToTangent = MIN_RADIUS / phi / v;
-fprintf('Time from O to T: %.2f seconds\n', timeToTangent);
-
-
-% FIND OPTIMAL R
-% distance = @(r, t) sqrt((jetX + r - r * cos(v * t / r) - bombX)^2 + (jetY + r * sin(v * t / r) - bombY)^2);
-% optimal_r = fminbnd(@(r) -distance(r, dropTime), MIN_RADIUS, 10000);
-% fprintf('The optimal r is: %.4f\n', optimal_r);
-% jetX = jetX + optimal_r - optimal_r * cos(v * t / optimal_r);
-% jetY = jetY + optimal_r * sin(v * t / optimal_r);
-% plot(jetX, jetY, 'o', 'Color', 'green');
-% FIND OPTIMAL R
-% optimal_r = fminbnd(@(r) -distance(r, dt), MIN_RADIUS, 10000);
-% fprintf('The optimal r is: %.4f\n', optimal_r);
+fprintf('Time from T to B: %.2f seconds\n', t - timeToTangent);
