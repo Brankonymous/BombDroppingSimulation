b6dc10d2edf95a5a651848250862f937ddf794e2
Igor Kandic <igorkandic2@gmail.com>
2024-05-24 16:57:31 +0200

    small change

--- shockwave_radius.m
+++ shockwave_radius.m
@@ -1,5 +1,7 @@
 function [R] = shockwave_radius(t)
     [~, ~, ~, BOMB_VEL, ~, ~] = constants();
-
+    
     R = BOMB_VEL * t;
+    R(R < 0) = 0;
+    
 end
--- simulation.m
+++ simulation.m
@@ -1,32 +1,52 @@
 [JET_H, JET_VEL, G, BOMB_VEL, MIN_RADIUS, dt] = constants();
 [dropTime, bombPosY] = bomb_position();
 
-ts = 0:0.1:dropTime;
+jetX = 0;
+jetY = 0; 
+bombX = 0;
+bombY = bombPosY;
+v = JET_VEL; 
+t = dropTime;  
+r = MIN_RADIUS;
+
+ts = 0:0.1:300;
 figure;
 hold on;
 grid on;
 axis([0 10000 -5000 5000 0 10000]);
 [xs, ys] = bomb_location(ts);
 
-rs=shockwave_radius(ts);
+% Find optimal radius
+distance = @(r, t) sqrt((jetX + r - r * cos(v * t / r) - bombX)^2 + (jetY + r * sin(v * t / r) - bombY)^2);
+optimal_r = fminbnd(@(r) -distance(r, t), MIN_RADIUS, 10000);
+
+plane_xs = optimal_r - optimal_r * cos(v * ts / optimal_r);
+plane_ys = optimal_r * sin(v * ts / optimal_r);
+
+fprintf('  The optimal r is: %.4f\n', optimal_r);
+
+
+rs=shockwave_radius(ts - dropTime);
+
 x0=bombPosY;
 y0=0;
 theta = linspace(0,2*pi,100);
-h = plot3(0,0,0,'.');
-for i = 1:length(ts)
-    set(h,'XData',xs(1:i),'YData',zeros(size(xs(1:i))), 'ZData', ys(1:i));
-    pause(0.01);
-end
-
 [X,Y,Z] = sphere;
+bomb_path = plot3(0,0,0,'.');
+plane_path = plot3(0,0,0,'.');
 for i = 1:length(ts)
+    if ts(i) < dropTime
+        set(bomb_path,'XData',xs(1:i),'YData',zeros(size(xs(1:i))), 'ZData', ys(1:i));
+    end
     r = rs(i);
-    
-    X2 = X * r;
-    Y2 = Y * r;
-    Z2 = Z * r;
+    if r > 0
+        X2 = X * r;
+        Y2 = Y * r;
+        Z2 = Z * r;
 
-    surf(X2+x0,Y2,Z2+y0)
+        surf(X2+x0,Y2,Z2+y0)
+    end
+    set(plane_path,'XData',plane_ys(1:i),'YData',-plane_xs(1:i)  ,'ZData',JET_H * ones(size(plane_xs(1:i))));
     pause(0.01);
 end
 
