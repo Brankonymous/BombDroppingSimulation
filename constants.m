8f2ebc320f596f6ef5d1406d823e5847c5cc7c18
Igor <igorkandic2@gmail.com>
2024-05-26 14:18:24 +0200

    small fix

--- constants.m
+++ constants.m
@@ -1,8 +1,8 @@
-function [JET_H, JET_VEL, G, BOMB_VEL, MIN_RADIUS, dt] = constants()
+function [JET_H, JET_VEL, G, EXPLOSION_VEL, MIN_RADIUS, dt] = constants()
     JET_H = 9600;
     JET_VEL = 530 * 1000 / (60 * 60);
     G = 9.81;
-    BOMB_VEL = 350;
+    EXPLOSION_VEL = 350;
     MIN_RADIUS = 4700;
-    dt = 0.001;
+    dt = 0.1;
 end
--- simulation.m
+++ simulation.m
@@ -1,17 +1,15 @@
 function [plane_xs, plane_ys, ts, xs, ys, rs] = simulation()
-    [JET_H, JET_VEL, G, BOMB_VEL, MIN_RADIUS, dt] = constants();
-    [drop_time, bombPosY] = bomb_position();
+    [JET_H, JET_VEL, G, EXPLOSION_VEL, MIN_RADIUS, dt] = constants();
+    [drop_time, detonation_y] = bomb_position();
 
-    hirosima_x = bombPosY;
     v = JET_VEL; 
-    t = drop_time;  
+    h = JET_H;
+    r = MIN_RADIUS;
 
-    ts = 0:0.2:300;
+    ts = 0:dt:300;
     [xs, ys] = bomb_location(ts);
 
-    r = 4700
-
-    phi = atan(r / hirosima_x);
+    phi = atan(r / detonation_y);
     tangent_x = r + r * cos(2 * phi);
     tangent_y = r * sin(2 * phi);
     time_to_tangent = r * (pi - 2 * phi) / v;
@@ -20,20 +18,18 @@ function [plane_xs, plane_ys, ts, xs, ys, rs] = simulation()
     plane_xs = r - r * cos(v * ts / r);
     plane_ys = r * sin(v * ts / r);
 
-    plane_xs(ts >= time_to_tangent) = tangent_x + sin(pi - 2 * phi) * v 
-                        * (ts(ts > time_to_tangent) - time_to_tangent);
-    plane_ys(ts >= time_to_tangent) = tangent_y + cos(pi - 2 * phi) * v 
-                        * (ts(ts > time_to_tangent) - time_to_tangent);
+    plane_xs(ts >= time_to_tangent) = tangent_x + sin(pi - 2 * phi) * v * (ts(ts > time_to_tangent) - time_to_tangent);
+    plane_ys(ts >= time_to_tangent) = tangent_y + cos(pi - 2 * phi) * v * (ts(ts > time_to_tangent) - time_to_tangent);
 
     rs = shockwave_radius(ts - drop_time);
 
-    vreme_sudara = 0;
+    time_of_impact = 0;
     for i = 1:length(ts)
-        if rs(i) > sqrt( (bombPosY - plane_ys(i))^2 + (0 + plane_xs(i))^2 + JET_H^2)
-            vreme_sudara = ts(i);
+        if rs(i) > sqrt( (detonation_y - plane_ys(i))^2 + (0 + plane_xs(i))^2 + h^2)
+            time_of_impact = ts(i);
             break
         end
     end
 
-    fprintf('  Vreme sudara: %.4f\n', vreme_sudara)
+    fprintf('  Vreme sudara: %.4f\n', time_of_impact);
 end
